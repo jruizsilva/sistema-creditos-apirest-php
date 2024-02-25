8da5fd7166016b7e09a4a650c6d330645fa5206d
jruizsilva <ruizsilvajonathan@gmail.com>
2024-02-25 00:46:09 -0300

    crud productos done

--- Controllers/Producto.php
+++ Controllers/Producto.php
@@ -8,13 +8,53 @@ class Producto extends Controllers
   }
 
 
-  public function producto($idProducto)
+  public function producto(string $idProducto)
   {
-    echo "Extraer un producto $idProducto";
+    try {
+      $method = $_SERVER['REQUEST_METHOD'];
+      if ($method === 'GET') {
+        if (empty($idProducto)) {
+          badRequestResponse("El id es requerido");
+        }
+        if (!is_numeric($idProducto)) {
+          badRequestResponse("El id no es valido");
+        }
+        $producto = $this->getModel()->findProductById($idProducto);
+        if (empty($producto)) {
+          notFoundResponse("Producto no encontrado");
+        }
+        $resSuccess = [
+          "status" => true,
+          "msg" => "Producto encontrado con exito",
+          "data" => $producto,
+        ];
+        jsonResponse($resSuccess);
+      } else {
+        badRequestResponse("Metodo no permitido");
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
+    die();
   }
   public function productos()
   {
-    echo "Extraer todos losproductos";
+    try {
+      $method = $_SERVER['REQUEST_METHOD'];
+      if ($method === 'GET') {
+        $productos = $this->getModel()->findAllProductos();
+        $resSuccess = [
+          "status" => true,
+          "msg" => "Productos encontrados con exito",
+          "data" => $productos,
+        ];
+        jsonResponse($resSuccess);
+      } else {
+        badRequestResponse("Metodo no permitido");
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
   }
   public function registro()
   {
@@ -72,13 +112,96 @@ class Producto extends Controllers
     }
     die();
   }
-  public function actualizar($idProducto)
+  public function actualizar(string $idProducto)
   {
-    echo "actualizar un producto: $idProducto";
+    try {
+      $method = $_SERVER['REQUEST_METHOD'];
+      if ($method == "PUT") {
+        if (empty($idProducto)) {
+          badRequestResponse("El id es requerido");
+        }
+        if (!is_numeric($idProducto)) {
+          badRequestResponse("El id no es valido");
+        }
+        $updateData = json_decode(file_get_contents('php://input'), true);
+        if (empty($updateData['codigo'])) {
+          badRequestResponse("El codigo es requerido");
+        }
+        if (empty($updateData['nombre'])) {
+          badRequestResponse("El nombre es requerido");
+        }
+        if (empty($updateData['descripcion'])) {
+          badRequestResponse("La descripcion es requerida");
+        }
+        if (empty($updateData['precio']) || !is_numeric($updateData['precio'])) {
+          badRequestResponse("El precio es requerido");
+        }
+        $codigo = strClean($updateData['codigo']);
+        $nombre = ucwords(strClean($updateData['nombre']));
+        $descripcion = strClean($updateData['descripcion']);
+        $precio = $updateData['precio'];
+
+        $producto = $this->getModel()->findProductById($idProducto);
+        if (empty($producto)) {
+          badRequestResponse("No existe el producto a actualizar");
+        }
+        $resUpdate = $this->getModel()->updateProducto($idProducto, $codigo, $nombre, $descripcion, $precio);
+
+        if ($resUpdate === false) {
+          badRequestResponse("El código del producto ya esta registrado");
+        }
+        if ($resUpdate === true) {
+          $datosProducto = [
+            'id_producto' => intval($idProducto),
+            'codigo' => $codigo,
+            'nombre' => $nombre,
+            'descripcion' => $descripcion,
+            'precio' => $precio,
+          ];
+          $resSuccess = [
+            "status" => true,
+            "msg" => "Producto actualizado con exito",
+            "data" => $datosProducto,
+          ];
+          jsonResponse($resSuccess, 200);
+        }
+
+      } else {
+        methodNotAllowedResponse($method);
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
   }
-  public function eliminar($idProducto)
+  public function eliminar(string $idProducto)
   {
-    echo "eliminar un producto: $idProducto";
+    try {
+      $method = $_SERVER['REQUEST_METHOD'];
+      if ($method == "DELETE") {
+        if (empty($idProducto)) {
+          badRequestResponse("El id es requerido");
+        }
+        if (!is_numeric($idProducto)) {
+          badRequestResponse("El id no es valido");
+        }
+        $producto = $this->getModel()->findProductById($idProducto);
+        if (empty($producto)) {
+          notFoundResponse("No existe el producto a eliminar");
+        }
+        $resDelete = $this->getModel()->deleteProducto($idProducto);
+        if ($resDelete === true) {
+          $resSuccess = [
+            "status" => true,
+            "msg" => "Producto eliminado con exito",
+          ];
+          jsonResponse($resSuccess, 200);
+        }
+      } else {
+        methodNotAllowedResponse($method);
+      }
+    } catch (Exception $e) {
+      echo $e->getMessage();
+    }
   }
 
 }
--- Models/ProductoModel.php
+++ Models/ProductoModel.php
@@ -2,7 +2,7 @@
 
 class ProductoModel extends Mysql
 {
-  private int $id_producto;
+  private string $id_producto;
   private string $codigo;
   private string $nombre;
   private string $descripcion;
@@ -35,11 +35,71 @@ class ProductoModel extends Mysql
 
   public function findProductByCode(string $codigo)
   {
-    $sqlSelect = "SELECT * FROM productos WHERE codigo = :codigo";
+    $sqlSelect = "SELECT * FROM productos WHERE codigo = :codigo AND status = 1";
     $valuesSelect = [':codigo' => $codigo];
 
     $producto = $this->select($sqlSelect, $valuesSelect);
 
     return $producto;
+  }
+
+  public function findProductById(string $id_producto)
+  {
+    $sqlSelect = "SELECT id_producto, codigo, nombre, descripcion, precio, DATE_FORMAT(date_created, '%d-%m-%Y') as fechaRegistro FROM productos WHERE id_producto = :id_producto AND status = 1";
+    $valuesSelect = [':id_producto' => $id_producto];
+
+    $producto = $this->select($sqlSelect, $valuesSelect);
+
+    return $producto;
+  }
+
+  public function updateProducto(string $id_producto, string $codigo, string $nombre, string $descripcion, float $precio): bool|int
+  {
+    $this->id_producto = $id_producto;
+    $this->codigo = $codigo;
+    $this->nombre = $nombre;
+    $this->descripcion = $descripcion;
+    $this->precio = $precio;
+
+    $sqlSelect = "SELECT * FROM productos WHERE codigo = :codigo AND id_producto != :id_producto";
+    $valuesSelect = [
+      ':codigo' => $this->codigo,
+      ':id_producto' => $this->id_producto
+    ];
+    $producto = $this->select($sqlSelect, $valuesSelect);
+    if (!empty($producto)) {
+      return false; // El código del producto ya existe en la base de datos.
+    }
+
+
+    $sqlUpdate = "UPDATE productos SET codigo = :codigo, nombre = :nombre, descripcion = :descripcion, precio = :precio WHERE id_producto = :id_producto";
+    $valuesUpdate = [
+      ':id_producto' => $this->id_producto,
+      ':codigo' => $this->codigo,
+      ':nombre' => $this->nombre,
+      ':descripcion' => $this->descripcion,
+      ':precio' => $this->precio
+    ];
+
+    $resUpdate = $this->update($sqlUpdate, $valuesUpdate);
+    return $resUpdate;
+  }
+
+  public function findAllProductos()
+  {
+    $sqlSelectAll = "SELECT id_producto, codigo, nombre, descripcion, precio, DATE_FORMAT(date_created, '%d-%m-%Y') as fechaRegistro FROM productos WHERE status = 1 ORDER BY id_product DESC";
+    $productos = $this->select_all($sqlSelectAll);
+    return $productos;
+  }
+
+  public function deleteProducto(string $id_producto): bool|int
+  {
+    $this->id_producto = $id_producto;
+
+    $sqlDelete = "UPDATE productos SET status = 0 WHERE id_producto = :id_producto";
+    $valuesDelete = [':id_producto' => $this->id_producto];
+
+    $resDelete = $this->update($sqlDelete, $valuesDelete);
+    return $resDelete;
   }
 }
